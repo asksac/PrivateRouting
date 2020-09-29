6e5c88b3c32a3c3f0c4e80e970119b260259221e
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-09-29 17:09:43 -0500

    Initial commit - working Python http.server

---
+++ src/webapp/server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+A simple HTTP Server in python for logging requests
+Usage:
+  ./server.py [<port>]
+"""
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from sys import argv
+import time
+import json 
+
+serverHost = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+    response = '''<html>
+      <head>
+        <title>AWS WebServer</title>
+      </head>
+      <body>
+        <p>Request Path: <pre>{}</pre></p>
+        <p>Client IP: <pre>{}</pre></p>
+        <p>Headers: <pre>{}</pre></p>
+      </body>
+    </html>'''
+    self.wfile.write(bytes(response.format(self.path, json.dumps(self.client_address), self.headers.as_string()), "utf-8"))
+
+if __name__ == "__main__": 
+  if len(argv) > 1:
+    serverPort = int(argv[1])
+
+  webServer = HTTPServer((serverHost, serverPort), MyServer)
+  print("Server started http://%s:%s" % (serverHost, serverPort))
+
+  try:
+    webServer.serve_forever()
+  except KeyboardInterrupt:
+    pass
+
+  webServer.server_close()
+  print("Server stopped.")
