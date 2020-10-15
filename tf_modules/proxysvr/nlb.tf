c9f88a0f4ff376b8e82475cfdfa544aed2cf47f4
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-10-15 17:08:29 -0500

    Moved endpoint service creation inside module

--- tf_modules/proxysvr/nlb.tf
+++ tf_modules/proxysvr/nlb.tf
@@ -1,52 +1,60 @@
 resource "aws_lb" "nlb" {
-  name                  = "${var.app_shortcode}-proxy-nlb-${substr(uuid(),0, 3)}"
-  internal              = true
-  load_balancer_type    = "network"
+  name                    = "${var.app_shortcode}-proxy-nlb-${substr(uuid(),0, 3)}"
+  internal                = true
+  load_balancer_type      = "network"
 
-  subnet_mapping {
-    subnet_id           = var.subnet_id
-  }
-
+  subnets                 = var.subnet_ids
+  
   lifecycle {
     create_before_destroy = true
     ignore_changes        = [name]
   }
 
-  tags                  = var.common_tags
+  tags                    = var.common_tags
 }
 
 resource "aws_lb_listener" "nlb_listeners" {
-  for_each              = var.proxy_config.port_mappings
+  for_each                = var.proxy_config.port_mappings
 
-  load_balancer_arn     = aws_lb.nlb.arn
-  port                  = each.value.nlb_port # inbound port of NLB
-  protocol              = "TCP"
+  load_balancer_arn       = aws_lb.nlb.arn
+  port                    = each.value.nlb_port # inbound port of NLB
+  protocol                = "TCP"
 
   default_action {
-    type                = "forward"
-    target_group_arn    = aws_lb_target_group.nlb_tgs[each.key].arn
+    type                  = "forward"
+    target_group_arn      = aws_lb_target_group.nlb_tgs[each.key].arn
   }
 }
 
 resource "aws_lb_target_group" "nlb_tgs" {
-  for_each              = var.proxy_config.port_mappings
+  for_each                = var.proxy_config.port_mappings
 
-  name                  = "${var.app_shortcode}-nlb-tg-${substr(uuid(),0, 3)}"
-  port                  = each.value.proxy_port # outbound port of NLB / inbound of targets
-  protocol              = "TCP"
+  name                    = "${var.app_shortcode}-nlb-tg-${substr(uuid(),0, 3)}"
+  port                    = each.value.proxy_port # outbound port of NLB / inbound of targets
+  protocol                = "TCP"
   # ref https://aws.amazon.com/premiumsupport/knowledge-center/target-connection-fails-load-balancer/
-  target_type           = "ip"
-  vpc_id                = var.vpc_id
+  target_type             = "ip"
+  vpc_id                  = var.vpc_id
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes        = [name]
+    #ignore_changes        = [name]
   }
 }
 
 resource "aws_lb_target_group_attachment" "nlb_tg_targets" {
-  for_each              = var.proxy_config.port_mappings
+  for_each                = var.proxy_config.port_mappings
 
-  target_group_arn      = aws_lb_target_group.nlb_tgs[each.key].arn
-  target_id             = aws_instance.proxy.private_ip
+  target_group_arn        = aws_lb_target_group.nlb_tgs[each.key].arn
+  target_id               = aws_instance.proxy.private_ip
+}
+
+resource "aws_vpc_endpoint_service" "vpces_nlb" {
+  acceptance_required         = false
+  network_load_balancer_arns  = [ aws_lb.nlb.arn ]
+  tags                        = merge(var.common_tags, map("Name", "${var.app_shortcode}_nlb_endpointsvc"))
+
+  lifecycle {
+    create_before_destroy     = true
+  }
 }
--- tf_modules/proxysvr/output.tf
+++ tf_modules/proxysvr/output.tf
@@ -2,10 +2,10 @@ output "proxysvr_dns" {
   value = aws_instance.proxy.private_dns
 }
 
-output "nlb_arn" {
-  value   = aws_lb.nlb.arn
-}
-
 output "nlb_dns" {
   value   = aws_lb.nlb.dns_name 
+}
+
+output "endpoint_service_name" {
+  value = aws_vpc_endpoint_service.vpces_nlb.service_name
 }
