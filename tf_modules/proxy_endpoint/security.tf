271d44bd1eaa8c5fde150cde88e3783633b1c264
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-10-25 22:11:44 -0500

    Replaced dynamic ingress block with sg rules

--- tf_modules/proxy_endpoint/security.tf
+++ tf_modules/proxy_endpoint/security.tf
@@ -2,6 +2,7 @@ resource "aws_security_group" "endpoint_sg" {
   name_prefix             = "${var.endpoint_name}-endpoint-sg"
   vpc_id                  = var.vpc.id
 
+  /*
   dynamic "ingress" {
     for_each              = var.proxy_config.port_mappings
     content {
@@ -11,6 +12,7 @@ resource "aws_security_group" "endpoint_sg" {
       protocol            = "tcp"
     }
   }
+  */
 
   egress {
     from_port             = 0
@@ -18,4 +20,16 @@ resource "aws_security_group" "endpoint_sg" {
     protocol              = "-1"
     cidr_blocks           = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group_rule" "endpoint_sg_rule" {
+  for_each                = var.proxy_config.port_mappings
+
+  type                    = "ingress"
+  security_group_id       = aws_security_group.endpoint_sg.id
+
+  cidr_blocks             = [ var.vpc.cidr_block ]
+  from_port               = each.value.nlb_port
+  to_port                 = each.value.nlb_port
+  protocol                = "tcp"
 }
