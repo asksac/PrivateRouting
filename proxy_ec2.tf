1cac7184c2acd6d4a58cf20cb37e42e4e05956f2
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-10-25 22:17:15 -0500

    Added new haproxy bypass listener/tg/rule to websvr

--- proxy_ec2.tf
+++ proxy_ec2.tf
@@ -41,6 +41,58 @@ module "proxy_ec2_endpoint" {
   common_tags             = local.common_tags
 }
 
+#
+# Addon listener bypassing HAProxy (vpc_endpoint -> nlb -> test_websvr)
+#
+
+resource "aws_security_group_rule" "vpce_addon_rule" {
+  type                    = "ingress"
+  security_group_id       = module.proxy_ec2_endpoint.security_group_id
+
+  cidr_blocks             = [ aws_vpc.vpc1.cidr_block ]
+  from_port               = var.addon_nlb_port
+  to_port                 = var.addon_nlb_port
+  protocol                = "tcp"
+
+  depends_on              = [ module.proxy_ec2_endpoint ]
+}
+
+resource "aws_lb_listener" "addon_listeners_to_websvr" {
+  load_balancer_arn       = module.proxy_ec2.nlb_arn
+  port                    = var.addon_nlb_port # inbound port of NLB
+  protocol                = "TCP"
+
+  default_action {
+    type                  = "forward"
+    target_group_arn      = aws_lb_target_group.addon_nlb_tg_to_websvr.arn
+  }
+
+  depends_on              = [ aws_security_group_rule.vpce_addon_rule ]
+}
+
+resource "aws_lb_target_group" "addon_nlb_tg_to_websvr" {
+  name                    = "${var.app_shortcode}-proxysvr-nlb-tg-addon-${substr(uuid(),0, 3)}"
+  port                    = var.addon_websvr_port # outbound port of NLB / inbound of targets
+  protocol                = "TCP"
+  target_type             = "ip"
+  vpc_id                  = aws_vpc.vpc2.id
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [ name ]
+  }
+}
+
+resource "aws_lb_target_group_attachment" "addon_nlb_tg_target" {
+  target_group_arn        = aws_lb_target_group.addon_nlb_tg_to_websvr.arn
+  target_id               = module.test_websvr.private_ip 
+  availability_zone       = aws_subnet.vpc3_subnet_pub1.availability_zone
+}
+
+#
+# Output
+#
+
 output "proxy_ec2" {
   value = {
     "nlb_dns"             = module.proxy_ec2.nlb_dns
@@ -48,13 +100,3 @@ output "proxy_ec2" {
     "endpoint_alias_dns"  = module.proxy_ec2_endpoint.alias_dns
   }
 }
-
-/*
-output "PEC2_vpc_endpoint_dns" {
-  value                   = module.proxy_ec2_endpoint.endpoint_dns 
-}
-
-output "PEC2_nlb_dns" {
-  value                   = module.proxy_ec2.nlb_dns
-}
-*/
