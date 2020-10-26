6aeb3daf8daa6b64982273f3fc0194eb33997412
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-10-25 22:10:00 -0500

    New output values: instance_id and private_ip

--- tf_modules/test_websvr/output.tf
+++ tf_modules/test_websvr/output.tf
@@ -2,10 +2,18 @@ output "private_dns" {
   value                   = aws_instance.websvr.private_dns
 }
 
+output "private_ip" {
+  value                   = aws_instance.websvr.private_ip
+}
+
 output "public_dns" {
   value                   = aws_instance.websvr.public_dns
 }
 
 output "alias_dns" {
   value                   = aws_route53_record.alias_dns.fqdn
+}
+
+output "instance_id" {
+  value                   = aws_instance.websvr.id
 }
