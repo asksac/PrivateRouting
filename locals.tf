43b2cbad865235927095bfc249d538f747ffde36
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-11-09 18:44:06 -0600

    Revised proxy config for ECS and EC2 clusters

--- locals.tf
+++ locals.tf
@@ -1,22 +1,34 @@
 locals {
   # Common tags to be assigned to all resources
   common_tags = {
-    Project     = "PrivateRouting"
-    Application = "AWS-OnPrem-Private-Routing"
-    Environment = "dev"
+    Application = var.app_name
+    Project     = "${var.app_name} - Demo and Test"
+    Environment = var.aws_env
   }
+
+  websvr_ports       = [ for p in range(0, 45): (8080 + p)]
+  proxy_mappings        = [ 
+    for p in local.websvr_ports: {
+      name              = "nginx${p}-http"
+      description       = "http port to websvr"
+      backend_host      = module.test_websvr.alias_dns
+      backend_port      = p 
+      nlb_port          = p
+      proxy_port        = p
+    }
+  ] 
 
   test_websvr_ports = {
     server_http_ports   = [ 80 ]
     server_https_ports  = [ 443 ]
     server_ssh_ports    = [ 22 ]
-    nginx_http_ports    = [ 8080, 8081, 8082, 8083, 8084 ]
-    nginx_https_ports   = [ 8443, 8444, 8445, 8446, 8447 ]
+    nginx_http_ports    = local.websvr_ports # [ 8080, 8081, 8082, 8083, 8084 ]
+    nginx_https_ports   = [ 8443 ] # [ 8443, 8444, 8445, 8446, 8447 ]
   }
 
   ecs_proxy_config = {
     service_name = "myproxy"
-    port_mappings = [
+    port_mappings = concat([
       {
         name          = "websvr-ssh"
         description   = "ssh connnection to websvr"
@@ -40,14 +52,6 @@ locals {
         backend_port  = 443
         nlb_port      = 7443
         proxy_port    = 7443
-      }, 
-      {
-        name          = "nginx1-http"
-        description   = "http connnection to nginx"
-        backend_host  = module.test_websvr.alias_dns
-        backend_port  = 8080
-        nlb_port      = 9080
-        proxy_port    = 9080
       }, 
       {
         name          = "nginx1-https"
@@ -57,44 +61,12 @@ locals {
         nlb_port      = 9443
         proxy_port    = 9443
       }, 
-      {
-        name          = "nginx2-http"
-        description   = "2nd http connnection to nginx"
-        backend_host  = module.test_websvr.alias_dns
-        backend_port  = 8081
-        nlb_port      = 9081
-        proxy_port    = 9081
-      }, 
-      {
-        name          = "nginx2-https"
-        description   = "2nd https connnection to nginx"
-        backend_host  = module.test_websvr.alias_dns
-        backend_port  = 8444
-        nlb_port      = 9444
-        proxy_port    = 9444
-      }, 
-      {
-        name          = "nginx3-http"
-        description   = "3rd http connnection to nginx"
-        backend_host  = module.test_websvr.alias_dns
-        backend_port  = 8082
-        nlb_port      = 9082
-        proxy_port    = 9082
-      }, 
-      {
-        name          = "nginx3-https"
-        description   = "3rd https connnection to nginx"
-        backend_host  = module.test_websvr.alias_dns
-        backend_port  = 8445
-        nlb_port      = 9445
-        proxy_port    = 9445
-      }, 
-    ]
+    ], local.proxy_mappings)
   }
 
   ec2_proxy_config = {
     service_name = "myec2proxy"
-    port_mappings = [
+    port_mappings = concat([
       {
         name          = "websvr-ssh"
         description   = "ssh connnection to websvr"
@@ -118,14 +90,6 @@ locals {
         backend_port  = 443
         nlb_port      = 7443
         proxy_port    = 7443
-      }, 
-      {
-        name          = "nginx1-http"
-        description   = "http connnection to nginx"
-        backend_host  = module.test_websvr.alias_dns
-        backend_port  = 8080
-        nlb_port      = 9080
-        proxy_port    = 9080
       }, 
       {
         name          = "nginx1-https"
@@ -135,6 +99,6 @@ locals {
         nlb_port      = 9443
         proxy_port    = 9443
       }
-    ]
+    ], local.proxy_mappings)
   }
 }
