37168f29a122978023c8123df67258184e8c1595
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-11-16 14:20:34 -0600

    Ports specified in websvr_http_ports reduced to 6

--- locals.tf
+++ locals.tf
@@ -6,9 +6,12 @@ locals {
     Environment = var.aws_env
   }
 
-  websvr_ports       = [ for p in range(0, 45): (8080 + p)]
-  proxy_mappings        = [ 
-    for p in local.websvr_ports: {
+  # Add '6' listen ports for websvr (can go upto 50 - 4 = 46)
+  websvr_http_ports     = [ for p in range(0, 6): (8080 + p)]
+
+  # Add new port mappings for each websvr listen port 
+  new_port_mappings = [ 
+    for p in local.websvr_http_ports: {
       name              = "nginx${p}-http"
       description       = "http port to websvr"
       backend_host      = module.test_websvr.alias_dns
@@ -22,7 +25,7 @@ locals {
     server_http_ports   = [ 80 ]
     server_https_ports  = [ 443 ]
     server_ssh_ports    = [ 22 ]
-    nginx_http_ports    = local.websvr_ports # [ 8080, 8081, 8082, 8083, 8084 ]
+    nginx_http_ports    = local.websvr_http_ports # [ 8080, 8081, 8082, 8083, 8084, 8085 ]
     nginx_https_ports   = [ 8443 ] # [ 8443, 8444, 8445, 8446, 8447 ]
   }
 
@@ -61,7 +64,7 @@ locals {
         nlb_port      = 9443
         proxy_port    = 9443
       }, 
-    ], local.proxy_mappings)
+    ], local.new_port_mappings)
   }
 
   ec2_proxy_config = {
@@ -99,6 +102,6 @@ locals {
         nlb_port      = 9443
         proxy_port    = 9443
       }
-    ], local.proxy_mappings)
+    ], local.new_port_mappings)
   }
 }
